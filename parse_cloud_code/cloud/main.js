44d479817ec11c3c50540d40d4c2d8b37f538bf5
Calvin Studebaker <calstud@stanford.edu>
2015-03-08 13:29:38 -0700

    have json data in cloud code function. no database connection yet.

--- parse_cloud_code/cloud/main.js
+++ parse_cloud_code/cloud/main.js
@@ -4,3 +4,45 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+
+
+
+Parse.Cloud.define("putMeterData", function(request, response){
+
+	var data = [];
+	var http = 
+
+	Parse.Cloud.httpRequest({
+		url: "https://data.sfgov.org/resource/7egw-qt89.json",
+		success: function(res){
+			data = res.data;
+			console.log("success");
+			response.success(data);
+		},
+		error: function(res){
+			console.error("request failed with response code: " + res.status);
+			response.error("bad");
+		}
+	});
+
+	
+});
+
+Parse.Cloud.define("test", function(request, response){
+	var test = require("cloud/test.js");
+	var val = test.isACoolName('Ralph');
+	response.success(val);
+});
+
+
+//from http://stackoverflow.com/questions/247483/http-get-request-in-javascript
+// function httpGet(theUrl)
+// {
+//     var xmlHttp = null;
+
+//     xmlHttp = new XMLHttpRequest();
+//     xmlHttp.open( "GET", theUrl, false );
+//     xmlHttp.send( null );
+//     return xmlHttp.responseText;
+// }
