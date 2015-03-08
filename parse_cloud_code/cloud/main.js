81308765b1cc0e9cd7a1426bbf085e086d1b5dae
Calvin Studebaker <calstud@stanford.edu>
2015-03-08 15:30:12 -0700

    attempts to save a parking entry to database

--- parse_cloud_code/cloud/main.js
+++ parse_cloud_code/cloud/main.js
@@ -6,27 +6,85 @@ Parse.Cloud.define("hello", function(request, response) {
 });
 
 
-
-
-Parse.Cloud.define("putMeterData", function(request, response){
-
-	var data = [];
-	var http = 
-
+Parse.Cloud.define("putSweepingData", function(request, response){
 	Parse.Cloud.httpRequest({
-		url: "https://data.sfgov.org/resource/7egw-qt89.json",
+		url: "http://parq.parseapp.com/sfsweeproutes.json",
 		success: function(res){
-			data = res.data;
-			console.log("success");
-			response.success(data);
+			var data = res.data;
+			var entry = data[0];
+			console.log("first entry: " + entry);
+			for (var key in entry){
+				var attrName = key;
+				var attrVal = entry[key];
+				// console.log(attrName + ": " + attrVal);
+			}
+			response.success("word");
 		},
 		error: function(res){
 			console.error("request failed with response code: " + res.status);
 			response.error("bad");
 		}
 	});
+});
 
-	
+Parse.Cloud.define("putMeterData", function(request, response){
+	var ParkingMeter = Parse.Object.extend("ParkingMeter");
+
+	console.log ("HERE");
+	Parse.Cloud.httpRequest({
+		url: "https://data.sfgov.org/resource/7egw-qt89.json",
+		success: function(res){
+			var data = JSON.parse(res.data);
+			for(i = 0; i < 1; i++){
+				var entry = new ParkingMeter();
+				var meter = data[i];
+				var streetname = meter["streetname"];
+				console.log(streetname);
+				var location = meter["location"];
+				var geopoint = new Parse.GeoPoint({latitude: location.latitude, longitude: location.longitude});
+
+
+				entry.set("has_active_sensor", meter.activesens);
+				entry.set("is_smart_meter", meter.smart_mete);
+				entry.set("location", geopoint);
+				entry.set("on_off_street", meter.on_off_str);
+				entry.set("street_number", meter.street_num);
+				entry.set("street_name", meter.streetname);
+				entry.set("meter_id", meter.post_id);
+
+				entry.save(null, {
+					succss: function(entry){
+
+					},
+					error: function(entry, error){
+						console.error("Failed to create new object, with error code: " + error.message);
+					}
+				});
+
+			}
+			// console.log(entry["activesens"]);
+			// console.log(entry["on_off_str"]);
+			// console.log(entry["sfparkarea"]);
+			// console.log(entry["location"]["longitude"]);
+			// console.log(entry["ratearea"]);
+			// console.log(entry["street_num"]);
+			// console.log(entry["street_seg"]);
+			// console.log(entry["ms_id"]);
+			// console.log(entry["cap_color"]);
+			// console.log(entry["post_id"]);
+			// console.log(entry["smart_mete"]);
+			// console.log(entry["meter_type"]);
+			// console.log(entry["jurisdicti"]);
+			// console.log(entry["osp_id"]);
+			// console.log(entry["ms_spaceid"]);
+			// console.log(entry["streetname"]);
+			response.success(data[0]);
+		},
+		error: function(res){
+			console.error("request failed with response code: " + res.status);
+			response.error("bad");
+		}
+	});
 });
 
 Parse.Cloud.define("test", function(request, response){
