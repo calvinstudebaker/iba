baa97b526cfab0aa25e882d883764ecc68bcc863
narekstan <narektovmasyan1705@gmail.com>
2015-05-28 04:34:56 -0700

    Coded against building a decreasing-frequency-based model for learning weights..

--- crimes_data/crime_convert.R
+++ crimes_data/crime_convert.R
@@ -2,7 +2,6 @@
 # Date: 05/18/2015
 
 setwd("~/iba/crimes_data/")
-#setwd("~/Desktop/CS 210/")
 
 install.packages('RJSONIO')
 library(RJSONIO)
@@ -10,7 +9,7 @@ stopifnot( isValidJSON('Crime.json') )
 
 crimeList <- fromJSON('Crime.json')
 #saveRDS(crimeList, file = "allCrimes.RData")
-crimeList = readRDS(file = "allCrimes.RData")
+#crimeList = readRDS(file = "allCrimes.RData")
 
 crimes.all = crimeList$results  # not idempotent!
 singleCrime = crimeList[[1]]
@@ -27,7 +26,6 @@ names(singleCrime$timestamp)  # "__type" "iso"
 #[1] -122.4144
 
 length(crimes.all)  # there are a total of 840,396 crime incidents!
-hundredThousandCrimes <- crimes.all[1:100000]
 
 
 #options(error = traceback)
@@ -42,7 +40,6 @@ toCSV <- function(selectedCrimes, filename) {
                     resolution = crime$resolution, timestamp = tstamp, updatedAt = crime$updatedAt)
     df
   }))
-  #saveRDS(result, "dfObj.RData")
   write.table(result, filename)
 }
 
@@ -51,14 +48,72 @@ processCrimes <- function(fromIndex, toIndex) {
   toCSV(crimes.all[fromIndex:toIndex], fname)
 }
 
-processCrimes(1, 1000)
 processCrimes(1, 10000)
 
 
 
 
+crTable <- read.table("crimes_1_10000.csv")
+
+########## Crime Description (cd) Freq. Analysis ###############
+
+cd.table <- table(crTable$descript)
+dim(cd.table) # hence:
+cd.table[order(cd.table)]
+# same as
+sort(cd.table)
+
+sort(cd.table, decreasing = T)
+# same as
+cd.table[order(-cd.table)]
+# same as
+cd.descending <- cd.table[order(cd.table, decreasing = T)] # an "array"
+
+cd.df <- as.data.frame(cd.descending)
+colnames(cd.df) = "Crime Freq."
+cd.text <- names(cd.table)
+#paste(cd.text, sep = "/") == cd.text
+#allText <- paste(cd.text, collapse = "   ")
+#nchar(allText) / length(cd.text)
+allWords <- paste(cd.text, collapse = " ")
+strsplit(allWords, split = " ")
 
 
-#rslt <- readRDS("dfObj.RData")
+
+########## Crime Type/Category (ct) Freq. Analysis ###############
+
+ct.table <- table(crTable$category)
+ct.descending <- sort(ct.table, decreasing = T)
+ct.df <- as.data.frame(ct.descending)
+colnames(ct.df) = "Crime Type Freq."
+
+subset(ct.df, `Crime Type Freq.` == 1)
+subset(ct.df, `Crime Type Freq.` > 1 & `Crime Type Freq.` < 10)
+subset(ct.df, `Crime Type Freq.` >= 10 & `Crime Type Freq.` < 100)
+subset(ct.df, `Crime Type Freq.` >= 100 & `Crime Type Freq.` < 1000)
+subset(ct.df, `Crime Type Freq.` >= 1000)
+
+
+
+########## Crime Address/Location Freq. Analysis ###############
+
+caddr.table <- table(crTable$address)
+dim(caddr.table)
+caddr.descending <- sort(caddr.table, decreasing = T)
+caddr.df <- as.data.frame(caddr.descending)
+colnames(caddr.df) = "Crime Address Freq."
+head(caddr.df, 50)
+
+cloc.table <- table(crTable$location)
+dim(cloc.table)
+cloc.descending <- sort(cloc.table, decreasing = T)
+cloc.df <- as.data.frame(cloc.descending)
+colnames(cloc.df) = "Crime Location Freq."
+head(cloc.df, 25)
+
+
+
+
+
 
 
