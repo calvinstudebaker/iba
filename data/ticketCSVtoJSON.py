b9240b904f6ab20ff4e05ca63a8eaa6ba8762fa6
Calvin Studebaker <calstud@stanford.edu>
2015-06-09 05:26:45 -0700

    commenting

--- data/ticketCSVtoJSON.py
+++ data/ticketCSVtoJSON.py
@@ -1,3 +1,16 @@
+#file: ticketCSVtoJSON.py
+#script to convert raw csv data for 2012 parking tickets (data/tickets.csv)
+#into a json file that can be imported into the Parse database. The csv
+#tickets only have an address, no lattitude/longitude GeoPoint, so this script
+#uses Google Maps API to geotag each ticket. Only 2500 API requests are allowed
+#per day, so this script is run everyday to geotag 2450 more tickets.
+#Usage: python ticketCSVtoJSON.py tickets.csv old_file.json new_file.json
+#First argument: tickets.csv is a csv file acquired from ParkRoulette that holds
+#every ticket issued in SF in 2012
+#Second argument: current json converted ticket data, this file will be the starting point
+#for new ticket json data, and will remain unchanged.
+#Third argument: destination file for new ticket json data.
+
 import csv
 import json
 import sys
@@ -36,18 +49,24 @@ fieldnames = (
 	"geopoint"
 )
 
+#create reader for the csv data
 reader = csv.DictReader( csvfile, fieldnames)
-rowCount = 0
+rowsRead = 0
+
+#load old json data (already-processed tickets)
 oldObject = json.load(oldjson)
 arr = oldObject["results"]
 oldRowCount = int(arr[len(arr)-1]["ticketId"])
 print "oldID = " + str(oldRowCount)
+
 try:
 	for row in reader:
 		if row["ticket_id"] == "ticket_id": continue #skip first line of csv
 		if int(row["ticket_id"]) <= oldRowCount: continue #get to next non-processed line
-		if rowCount > 2450 : break	#limit API calls to not overflow daily limit
-		print "rowCount = " + str(rowCount) + " id = " + row["ticket_id"] #print progress
+		if rowsRead > 2450 : break	#limit API calls to not overflow daily limit
+		print "rowsRead = " + str(rowsRead) + " id = " + row["ticket_id"] #print progress
+
+		#create new ticket row
 		d = dict()
 		csvDate = row["issue_datetime"]
 		tokens = csvDate.split()
@@ -63,6 +82,7 @@ try:
 		address = row["location"]
 		d["address"] = address
 
+		#geotag the ticket with a GeoPoint
 		url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + address.replace(" ", "+") + ",+San+Francisco"
 		response = urllib2.urlopen(url)
 		data = json.load(response)
@@ -78,9 +98,10 @@ try:
 			print(data)
 			if data['status'] == 'OVER_QUERY_LIMIT': break
 
-		rowCount+=1
+		rowsRead+=1
 		#pause to not exceed google api usage limits
 		time.sleep(2)
 finally:
+	#save results in new json file
 	json.dump({"results" : arr}, newjson)
 	print("done")
