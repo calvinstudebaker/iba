6466af85f1e31f0933a60b2d443f36e5302cd1ff
narekstan <narektovmasyan1705@gmail.com>
2015-06-10 22:23:03 -0700

    Thoroughly commented all the functions, giving concrete directions for running the codebase and reproducing the crime types' weight-learning/calculation results.

--- crimes_data/learn_weights.R
+++ crimes_data/learn_weights.R
@@ -1,43 +1,27 @@
-# Author: Narek Tovmasyan (Team IBA)
-# Date: 05/28/2015
+# *------------------------------------------------------------------
+# | PROGRAM NAME: learn_weights.R
+# | DATE: 05/28/2015
+# | CREATED BY: Narek Tovmasyan (Team IBA)
+# | PROJECT FILE: PARQ
+# *---------------------------------------------------
 
 
+# Corresponds to the GitHub repo structure @ https://github.com/cs210/iba
 setwd("~/iba/crimes_data/")
 
-
-########## Crime Description (cd) Freq. Analysis ###############
-
-cd.table <- table(crTable$descript)
-dim(cd.table) # hence:
-cd.table[order(cd.table)]
-
-cd.table[order(-cd.table)]
-# same as
-cd.descending <- cd.table[order(cd.table, decreasing = T)] # an "array"
-
-cd.df <- as.data.frame(cd.descending)
-colnames(cd.df) = "Crime Freq."
-cd.text <- names(cd.table)
-#paste(cd.text, sep = "/") == cd.text
-#allText <- paste(cd.text, collapse = "   ")
-#nchar(allText) / length(cd.text)
-allWords <- paste(cd.text, collapse = " ")
-strsplit(allWords, split = " ")
-
-
-
-
-
-setwd("~/iba/crimes_data/")
 crTable <- read.table("crimes_1_10000.csv")
 
 setwd("~/Desktop/CS 210/")
+# Or, set it to the folder where the CSV file with a large number of crimes is
 crTable1 <- read.table("crimes_10001_50000.csv")
-crimeTabular.all <- rbind(crTable, crTable1)
 
-crTable <- crimeTabular.all
+crime.table.all <- rbind(crTable, crTable1)
 
-########## Crime Type/Category (ct) Freq. Analysis ###############
+crTable <- crime.table.all
+
+
+
+########## Crime Type/Category (ct) Frequency Analysis ##########
 
 ct.table <- table(crTable$category)
 stopifnot( sum(ct.table) == dim(crTable)[1] )
@@ -45,9 +29,10 @@ ct.descending <- sort(ct.table, decreasing = T)
 ct.df <- as.data.frame(ct.descending)
 colnames(ct.df) = "Crime Type Freq."
 
-print( paste("There are found to be a total of", nrow(ct.df), "crime types!") )
+m = nrow(ct.df)
+print( paste("There are found to be a total of", m, "crime types!") )
 
-############################ The BENCHMARK #######################
+############### Establishing The Benchmark ###############
 subset(ct.df, `Crime Type Freq.` == 1)
 subset(ct.df, `Crime Type Freq.` > 1 & `Crime Type Freq.` < 10)
 subset(ct.df, `Crime Type Freq.` >= 10 & `Crime Type Freq.` < 100)
@@ -56,9 +41,22 @@ subset(ct.df, `Crime Type Freq.` >= 1000)
 
 
 
-########## Crime Address/Location Freq. Analysis ###############
+########## Crime Address/Location Frequency Analysis ##########
 
-crime.freq.by <- function(crimeSpreadSheet, addr.geoloc.toggle = "address", headCount = 25) {
+####
+# Computes the crime frequency of each unique location and displays the
+# supplied number of top (higher-frequency) ones, after sorting.
+#
+# @param crimeSpreadSheet: data frame, each row describing a crime incident.
+# @param addr.geoloc.toggle: character, to switch between address and geo-location.
+# @param headCount: integer, the # of top "hot" addresses/geo-locations for crime.
+#
+# @return: data frame, the whole, sorted crime location - crime frequency table.
+#
+# @examples
+# CrimeFreqBy(crimesCSV, "location", 5)
+##
+CrimeFreqBy <- function(crimeSpreadSheet, addr.geoloc.toggle = "address", headCount = 25) {
   if (addr.geoloc.toggle == "address") {
     caddr.table <- table(crimeSpreadSheet$address)
   } else if (addr.geoloc.toggle == "location") {
@@ -91,23 +89,35 @@ crime.freq.by <- function(crimeSpreadSheet, addr.geoloc.toggle = "address", head
   }
 }
 
-caddr.df <- crime.freq.by(crTable, "address", 10)
+caddr.df <- CrimeFreqBy(crTable, "address", 10)
 
-########## Converthing Into Probabilities crime-category-wise ###############
 
-N = nrow(caddr.df)
+
+########## Converthing Into Probabilities Crime-Category-Wise ##########
+
+n = nrow(caddr.df)
+N = sum(caddr.df)
 probability.any.incident <- caddr.df / N
 
-# Now, address-/location-based prob. calc.'s for every crime type..
-#singleAddr <- caddr.df[93,]
+# Now, address- / location-based probability calculations for every crime type..
 
-all.types.Probs <- function(singleAddr) {
+####
+# Estimates all the recognized crime types' probabilities for the given address.
+#
+# @param singleAddr: "named" integer, a single address + crimes' count.
+#
+# @return: table, having a probability value for each crime type.
+# @note: These are joint probability values, not conditional probabilities!
+#
+# @examples
+# ProbsOfAllTypes(caddr.df[7, ])
+##
+ProbsOfAllTypes <- function(singleAddr) {
   singleAddr <- names(singleAddr)
   ss.in.addr <- subset(crTable, address == singleAddr)
 
   # Confirming it keeps all the (crime) factor levels..
-  length( levels(ss.in.addr$category) ) == nrow(ct.df)
-  m = nrow(ct.df)
+  stopifnot( length( levels(ss.in.addr$category) ) == m )
 
   tt <- table(ss.in.addr$category)
   # Doing Laplace smoothing..
@@ -116,25 +126,22 @@ all.types.Probs <- function(singleAddr) {
   # Ensuring these are valid probability values..
   #stopifnot( sum(probs.crime.types) == 1 )
 
-  probOfAnything = probability.any.incident[singleAddr,]
+  probOfAnything <- probability.any.incident[singleAddr, ]
   names(probOfAnything) = NULL
 
-  # Finally -- reporting all crime-type prob.'s for this single address!
-  pTable = probOfAnything * probs.crime.types
+  # Reporting all crime-type probabilities for this single address:
+  pTable <- probOfAnything * probs.crime.types
   pTable
 }
 
 
-#all.addr.all.crimes = matrix(nrow = 0, ncol = nrow(ct.df))
-#apply(caddr.df, 1, function(x) {
 allAddresses = rownames(caddr.df)
 allCrimeTypes = rownames(ct.df)
-all.addr.all.crimes = matrix(nrow = nrow(caddr.df), ncol = nrow(ct.df), 
+all.addr.all.crimes = matrix(nrow = n, ncol = m, 
                              dimnames = list(allAddresses, allCrimeTypes))
-for (idx in 1:nrow(caddr.df)) {
-  currRow = caddr.df[idx,]
-  #all.addr.all.crimes <- rbind(all.addr.all.crimes, all.types.Probs(x))
-  all.addr.all.crimes[idx,] <- all.types.Probs(currRow)
+for (idx in 1:n) {
+  currRow = caddr.df[idx, ]
+  all.addr.all.crimes[idx, ] <- ProbsOfAllTypes(currRow)
 }
 all.addr.all.crimes
 
@@ -144,35 +151,57 @@ crimeCategChances <- sort(likelihoods.crime.category, decreasing = T)
 likelihoods.crime.type <- as.data.frame(crimeCategChances)
 names(likelihoods.crime.type) <- "Likelihood of Crime Type"
 
-probs.crime.type <- likelihoods.crime.type / sum(likelihoods.crime.type)
-stopifnot( sum(probs.crime.type) <= 1 )
-names(probs.crime.type) <- "Probability of Crime Type"
+total <- sum(likelihoods.crime.type)
+crime.types.aggregate.probs <- likelihoods.crime.type / total
+stopifnot( sum(crime.types.aggregate.probs) == 1 )
+names(crime.types.aggregate.probs) <- "Probability of Crime Type"
 
-write.table(probs.crime.type, file = "probOfEveryCrimeType.csv")
-
-
-# So called un-likelihood (seriousness) of each crime..
-# We achieve that by inverting the probability values:
-inverseProbs <- 1 / probs.crime.type
-names(inverseProbs) <- "Seriousness of Crime"
-
-lhOfRange <- as.numeric( head(inverseProbs, 1) )
-rhOfRange <- as.numeric( tail(inverseProbs, 1) )
-rangeSize = rhOfRange - lhOfRange
-
-# the size of each weight "step" (in the scale of 1 through 10)..
-stepsize = rangeSize / 9
-weights <- 1 + (inverseProbs - lhOfRange) / stepsize
-names(weights) <- "Weight of Crime Type"
-
-#write.table(weights, file = "crimeTypeWeights.csv")
-
-write.table(weights, file = "~/iba/crimeTypeWeights_50000.csv")
+# Write to disk, locally!
+setwd("~/Desktop/CS 210/")
+write.table(crime.types.aggregate.probs, file = "aggregateChanceOfEveryCrimeType.csv")
 
 
+####
+# Determines (on the scale 1 through 10) the "weight" of each crime type,
+# higher value denoting a more dangerous crime.
+#
+# @param overallChanceOfEveryCrimeType: data frame, self-explanatory.
+#
+# @return: table, having a net weight value (crime score) for each crime type.
+##
+WeighEachCrimeType <- function(overallChanceOfEveryCrimeType) {
+  # So called un-likelihood (seriousness) of each crime..
+  # We achieve that by inverting the probability values:
+  inverseProbs <- 1 / overallChanceOfEveryCrimeType
+  names(inverseProbs) <- "Seriousness of Crime"
+
+  lhOfRange <- as.numeric( head(inverseProbs, 1) )
+  rhOfRange <- as.numeric( tail(inverseProbs, 1) )
+  rangeSize <- rhOfRange - lhOfRange
+
+  # the size of each weight "step" (in the scale of 1 through 10)..
+  stepsize = rangeSize / 9
+  weights <- 1 + (inverseProbs - lhOfRange) / stepsize
+  names(weights) <- "Weight of Crime Type"
+  weights
+}
+learnedWeights <- WeighEachCrimeType(crime.types.aggregate.probs)
+
+#write.table(learnedWeights, file = "~/iba/crimes_data/learnedWeights.csv")
 
 
-###################### CLUSTERING METHOD ########################
+
+
+
+
+
+
+
+
+
+
+
+#################### CLUSTERING METHOD ####################
 
 # Now we define 3 clusters / high-level "categories" of crime types 
 # on which to do the samy probabilistic method-based weight estimation, individually..
@@ -208,9 +237,43 @@ print( paste("There are", length(category.III), "crime types of high-level Categ
 
 
 
-ct.array.I <- ct.df[category.I,]
+ct.array.I <- ct.df[category.I, ]
 ct.sorted.I <- sort(ct.array.I, decreasing = T)
 ct.df.I <- as.data.frame(ct.sorted.I)
 colnames(ct.df.I) = "Crime Type Freq."
+
+ct.array.II <- ct.df[category.II, ]
+ct.sorted.II <- sort(ct.array.II, decreasing = T)
+ct.df.II <- as.data.frame(ct.sorted.II)
+colnames(ct.df.II) = "Crime Type Freq."
+
+ct.array.III <- ct.df[category.III, ]
+ct.sorted.III <- sort(ct.array.III, decreasing = T)
+ct.df.III <- as.data.frame(ct.sorted.III)
+colnames(ct.df.III) = "Crime Type Freq."
+
+
+
+
+
+
+############### Crime Description (cd) Freq. Analysis ###############
+
+cd.table <- table(crTable$descript)
+dim(cd.table) # hence:
+cd.table[order(cd.table)]
+
+cd.table[order(-cd.table)]
+# same as
+cd.descending <- cd.table[order(cd.table, decreasing = T)] # an "array"
+
+cd.df <- as.data.frame(cd.descending)
+colnames(cd.df) = "Crime Freq."
+cd.text <- names(cd.table)
+#paste(cd.text, sep = "/") == cd.text
+#allText <- paste(cd.text, collapse = "   ")
+#nchar(allText) / length(cd.text)
+allWords <- paste(cd.text, collapse = " ")
+strsplit(allWords, split = " ")
 
 
